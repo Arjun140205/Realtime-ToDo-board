989f59433dd61eaa263834c350e43a5f96ff973f
Arjunbir Singh <arjunbirsingh1699@gmail.com>
2025-07-12 23:44:51 +0530

    Updated API utility functions to handle responses more effectively

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api', // change for deployment
+  baseURL: 'http://localhost:5001/api', // change for deployment
 });
 
 API.interceptors.request.use((config) => {
