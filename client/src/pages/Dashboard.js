e0f3ac132a269190668c9de2181876fcd80cde80
Arjunbir Singh <arjunbirsingh1699@gmail.com>
2025-07-12 23:44:51 +0530

    Added Dashboard page to display user tasks and project overview

---
+++ client/src/pages/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div className="dashboard">
+      <h1>Todo Board Dashboard</h1>
+      <p>Welcome to your todo board!</p>
+      {/* TODO: Add todo board functionality */}
+    </div>
+  );
+};
+
+export default Dashboard;
