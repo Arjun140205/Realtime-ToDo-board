b47922803365076e3651ec1fb7362375f1641b9d
Arjunbir Singh <arjunbirsingh1699@gmail.com>
2025-07-13 00:45:19 +0530

    Updated App.js to integrate new components and improve routing flow

--- client/src/App.js
+++ client/src/App.js
@@ -3,19 +3,29 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
 import ProtectedRoute from './components/ProtectedRoute';
+import Navbar from './components/Navbar';
+import About from './pages/About';
+import Footer from './components/Footer';
 
 const App = () => {
+  const location = window.location.pathname;
+  const hideNavFooter = ["/", "/login", "/register"].includes(location);
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/dashboard" element={
-        <ProtectedRoute>
-          <Dashboard />
-        </ProtectedRoute>
-      } />
-    </Routes>
+    <>
+      {!hideNavFooter && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        } />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      {!hideNavFooter && <Footer />}
+    </>
   );
 };
 
