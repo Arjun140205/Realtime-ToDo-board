5961723e40f97639bdd3980d763ac0a3e418fbca
Arjunbir Singh <arjunbirsingh1699@gmail.com>
2025-07-12 23:54:26 +0530

    Updated Dashboard layout and logic to support real-time task collaboration

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,11 +1,51 @@
-import React from 'react';
+import { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../context/AuthContext';
+import TaskBoard from '../components/TaskBoard';
+import API from '../utils/api';
+import socket from '../utils/socket';
 
 const Dashboard = () => {
+  const { user, logout } = useContext(AuthContext);
+  const [tasks, setTasks] = useState([]);
+
+  const fetchTasks = async () => {
+    const res = await API.get('/tasks');
+    setTasks(res.data);
+  };
+
+  useEffect(() => {
+    fetchTasks();
+
+    // socket event listeners
+    socket.on('taskCreated', (newTask) => {
+      setTasks((prev) => [...prev, newTask]);
+    });
+
+    socket.on('taskUpdated', (updatedTask) => {
+      setTasks((prev) =>
+        prev.map((task) => (task._id === updatedTask._id ? updatedTask : task))
+      );
+    });
+
+    socket.on('taskDeleted', (taskId) => {
+      setTasks((prev) => prev.filter((task) => task._id !== taskId));
+    });
+
+    return () => {
+      socket.off('taskCreated');
+      socket.off('taskUpdated');
+      socket.off('taskDeleted');
+    };
+  }, []);
+
   return (
-    <div className="dashboard">
-      <h1>Todo Board Dashboard</h1>
-      <p>Welcome to your todo board!</p>
-      {/* TODO: Add todo board functionality */}
+    <div>
+      <header>
+        <h1>Welcome, {user.name}</h1>
+        <button onClick={logout}>Logout</button>
+      </header>
+
+      <TaskBoard tasks={tasks} setTasks={setTasks} />
     </div>
   );
 };
