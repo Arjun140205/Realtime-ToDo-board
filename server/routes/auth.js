04f042bdba1e8215fc6686261e1bea77fae7998d
Arjunbir Singh <arjunbirsingh1699@gmail.com>
2025-07-15 00:13:18 +0530

    modified auth.js routes to streamline login and signup flow

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,11 +2,12 @@
 
 const express = require('express');
 const router = express.Router();
-const { registerUser, loginUser, getProfile } = require('../controllers/authController');
+const { registerUser, loginUser, getProfile, listUsers } = require('../controllers/authController');
 const { protect } = require('../middleware/authMiddleware');
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.get('/me', protect, getProfile);
+router.get('/users', listUsers); // TEMP: List all users
 
 module.exports = router;
